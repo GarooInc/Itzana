815a83877d71ff6a95e1ebe8af00c3b0deaea513
Jimena Hernández <her21199@uvg.edu.gt>
2024-07-29 11:54:23 -0600

    Update Carousel.jsx

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -12,7 +12,7 @@ const Carousel = ({images}) => {
                             index !== 0 && <a href={`#slide-${index - 1}`} className="absolute bg-aqua text-white px-4 py-2 rounded-full left-5">❮</a>
                         }
                         {
-                            index !== images.length - 1 && <a href={`#slide-${index + 1}`} className="absolute bg-aqua text-white px-4 py-2 rounded-full right-5">❯</a>
+                            index !== images.length - 1 && <a href={`#slide-${index}`} className="absolute bg-aqua text-white px-4 py-2 rounded-full right-5">❯</a>
                         }
                     </div>
                 </div> 
