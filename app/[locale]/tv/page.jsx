8ea7c5dc2ec66f5e5c48b0ce3bb975d57f2319ca
Jimena Hernández <her21199@uvg.edu.gt>
2024-08-05 09:17:30 -0600

    Update page.jsx

--- app/[locale]/tv/page.jsx
+++ app/[locale]/tv/page.jsx
@@ -15,7 +15,7 @@ return (
     <TranslationsProvider locale={locale} namespaces={namespaces} resources={resources}>
         <div className="page bg-white">
             <div className="flex flex-col w-full h-screen items-center">
-                <HeaderItem namePage={t('header:activities')} />
+                <HeaderItem namePage="TV" />
                 <div className="md:w-1/2 w-3/4  bg-white rounded-lg shadow p-4 my-10 font-futura text-black text-center">
                     Find ocean-view rooms, eclectic cuisine, and sparkling pools along the Caribbean Sea. Explore our all-inclusive resort ahead of your stay and
                     imagine what&apos;s possible.
