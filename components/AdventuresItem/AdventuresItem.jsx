a891686a93d454f1095e22be52ef3e2867377d70
Jimena Hernández <her21199@uvg.edu.gt>
2024-08-05 11:36:31 -0600

    Update AdventuresItem.jsx

--- components/AdventuresItem/AdventuresItem.jsx
+++ components/AdventuresItem/AdventuresItem.jsx
@@ -27,13 +27,14 @@ const AdventuresItem = () => {
 
 
     return (
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 grid-flow-row-dense auto-rows-auto py-20 md:px-10">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 grid-flow-row-dense auto-rows-auto py-10 md:px-10">
             {adventures.map((item, index) => (
-                <div key={index} className={`bg-white px-2 pb-12 shadow rounded-lg gap-2 flex flex-col relative`}>
-                    <img className="md:w-full md:h-32 w-40 h-48 md:h-52 rounded-lg object-cover" src={`https://kaana.garooinc.com/kaana/api/files/${item.collectionId}/${item.id}/${item.image}?token=`} alt={item.name} />
-                    <h3 className="text-black text-base leading-tight font-futura mt-2">{item.title}</h3>
-                    <p className="text-black text-xs font-[futura light] leading-none">{item.short_Description}</p>
+                <div key={index} className={`bg-white px-2 pb-16 shadow rounded-lg gap-2 flex flex-col relative`}>
+                    <img className="md:w-full md:h-32 w-full h-48 md:h-52 rounded-lg object-cover" src={`https://kaana.garooinc.com/kaana/api/files/${item.collectionId}/${item.id}/${item.image}?token=`} alt={item.name} />
+                    <h3 className="text-black text-xl leading-tight font-futura mt-2">{item.title}</h3>
+                    <p className="text-black text-md font-futuralight leading-none">{item.short_Description}</p>
                     <p className="text-lightgray text-xs font-light leading-none font-futura absolute bottom-2 left-2"> £{item.price}</p>
+                    <button className='text-white bg-aqua p-2 rounded  absolute bottom-2 right-2 font-futura'>Request a reservation</button>
                 </div>
             ))}
         </div>
