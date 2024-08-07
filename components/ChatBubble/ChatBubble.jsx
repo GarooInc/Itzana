c2b6c4c774dbd21a226ebcd1e4ad1b62e2649059
Jimena Hernández <her21199@uvg.edu.gt>
2024-08-07 09:49:53 -0600

    Update ChatBubble.jsx

--- components/ChatBubble/ChatBubble.jsx
+++ components/ChatBubble/ChatBubble.jsx
@@ -4,15 +4,17 @@ import { TbHandClick } from "react-icons/tb"
 const ChatBubble = () => {
   return (
     <div className="fixed bottom-4 cursor-pointer">
+      <a href="https://wa.me/5016101329" target="_blank" rel="noopener noreferrer">
         <div className="w-56 h-10 relative">
-            <div className="w-56 h-9 left-[1px] top-[2px] absolute bg-darkgray rounded-tl-2xl rounded-tr rounded-bl-2xl rounded-br">
-            </div>
-            <div className="left-[49px] top-[10px] absolute text-white text-base font-medium font-futura leading-tight">Chat with concierge</div>
-            <div className="w-10 h-10 left-0 top-0 absolute bg-darkgray rounded-full shadow-inner"></div>
-            <div className="w-6 h-6 left-[8px] top-[8px] absolute">
-                <TbHandClick className="w-6 h-6 left-[2px] top-[2px] absolute text-white" />
-            </div>
+          <div className="w-56 h-9 left-[1px] top-[2px] absolute bg-darkgray rounded-tl-2xl rounded-tr rounded-bl-2xl rounded-br">
+          </div>
+          <div className="left-[49px] top-[10px] absolute text-white text-base font-medium font-futura leading-tight">Chat with concierge</div>
+          <div className="w-10 h-10 left-0 top-0 absolute bg-darkgray rounded-full shadow-inner"></div>
+          <div className="w-6 h-6 left-[8px] top-[8px] absolute">
+            <TbHandClick className="w-6 h-6 left-[2px] top-[2px] absolute text-white" />
+          </div>
         </div>
+      </a>
     </div>
   )
 }
