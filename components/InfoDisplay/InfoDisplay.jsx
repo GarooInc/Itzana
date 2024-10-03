b4bb13b733829c4696bc14b99fb72a52f67390a6
Jimena Hernández <her21199@uvg.edu.gt>
2024-10-03 15:13:43 -0600

    update

--- components/InfoDisplay/InfoDisplay.jsx
+++ components/InfoDisplay/InfoDisplay.jsx
@@ -2,10 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import PocketBase from 'pocketbase';
 import { useTranslation } from 'react-i18next';
-import useFormattedText from '@/hooks/useFormattedText';
 
-
-const InfoDisplay = ({collection}) => {
+const InfoDisplay = ({ collection }) => {
     const [data, setData] = useState([]);
     const pb = new PocketBase('https://kaana.garooinc.com/kaana');
     pb.autoCancellation(false);
@@ -13,42 +11,40 @@ const InfoDisplay = ({collection}) => {
     const { i18n } = useTranslation();
     const currentLocale = i18n.language;
 
-    
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const records = await pb.collection(collection).getFullList({
-                    sort: '-created',
+                    sort: id_number,
                 });
                 setData(records);
-            }
-            catch (error) {
+            } catch (error) {
                 console.error("Error fetching data: ", error);
             }
-        }
+        };
         fetchData();
     }, []);
 
+    return (
+        <div className="flex flex-col justify-center items-center gap-4 py-10 md:w-3/4 px-10">
+            {
+                data.map((item, index) => (
+                    <div className="collapse collapse-arrow border-b border-cream w-full rounded-none" key={index}>
+                        {/* Cambiado a checkbox */}
+                        <input type="checkbox" className="peer" />
+                        <div className="collapse-title text-xl font-medium text-white font-tiempos peer-checked:collapse-open">
+                            {item[`title_${currentLocale}`]}
+                        </div>
+                        <div className="collapse-content peer-checked:collapse-open">
+                            <p className='text-white font-futura infodisplay'
+                                dangerouslySetInnerHTML={{ __html: item[`desc_${currentLocale}`] }}>
+                            </p>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
 
-  return (
-    <div className='flex flex-col justify-center items-center gap-4 py-10 md:w-3/4 px-10'>
-    {
-        data.map((item, index) => (
-            <div className="collapse collapse-arrow border-b border-cream w-full rounded-none" key={index}>
-            <input type="radio" name="my-accordion-2" defaultChecked className='rounded-none'/>
-            <div className="collapse-title text-xl font-medium text-white font-tiempos">
-                {item[`title_${currentLocale}`]}
-            </div>
-            <div className="collapse-content">
-                <p className='text-white font-futura'
-                dangerouslySetInnerHTML={{ __html: item[`desc_${currentLocale}`] }}>
-                </p>
-            </div>
-            </div>
-        ))
-    }
-    </div>
-  )
-}
-
-export default InfoDisplay
+export default InfoDisplay;
