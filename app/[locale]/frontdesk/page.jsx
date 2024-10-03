3b7ee4b59f0c6a21d667cedc541fb6acd1582c06
Jimena Hernández <her21199@uvg.edu.gt>
2024-10-03 12:40:09 -0600

    Update page.jsx

--- app/[locale]/frontdesk/page.jsx
+++ app/[locale]/frontdesk/page.jsx
@@ -4,6 +4,7 @@ import TranslationsProvider from '@/components/TranslationsProvider'
 import LanguageSwitcher from '@/components/LanguageSwitcher/LanguageSwitcher'
 import HeaderItem from '@/components/HeaderItem/HeaderItem'
 import InfoDisplay from '@/components/InfoDisplay/InfoDisplay'
+import { FaWhatsapp, FaPhone, FaClock, FaWifi } from "react-icons/fa"
 
 const namespaces = ['home', 'header']
 
@@ -15,6 +16,35 @@ export default async function FrontDesk({ params: { locale }}) {
         <div className="page bg-green">
             <HeaderItem v={"v5"} />
             <h2 className='italictiempos_title'>{t('home:nav2')}</h2>
+            <div className="bg-cream p-6 rounded-lg font-serif flex flex-col gap-4 mx-10">
+                <div className='flex md:flex-row flex-col items-center gap-4'>
+                    <div className="flex items-center mt-6">
+                        <FaWhatsapp className="mr-2 text-green text-2xl" />
+                        <span className="md:text-lg text-gray-700">WhatsApp: +501 610 1329</span>
+                    </div>
+                    <div className="flex items-center mt-4">
+                        <FaPhone className="mr-2 text-green text-2xl" />
+                        <span className="md:text-lg text-gray-700">Extension: Dial Ext #1000</span>
+                    </div>
+                </div>
+
+                <div className="flex md:flex-row flex-col items-center mt-4 gap-4">
+                    <div className="flex items-center mt-4">
+                        <FaWifi className="mr-2 text-green text-2xl" />
+                        <span className="md:text-lg text-gray-700">Wi-Fi Network: ItzanaGuest</span>
+                    </div>
+                    <div className="flex items-center mt-4">
+                        <FaWifi className="mr-2 text-green text-2xl" />
+                        <span className="md:text-lg text-gray-700">Wi-Fi Password: Itzana123</span>
+                    </div>
+                </div>
+
+                <div className="flex items-center mt-4">
+                    <FaClock className="mr-2 text-green text-2xl" />
+                    <span className="md:text-lg text-gray-700">Hours: From 6:00 am to 10:00 pm</span>
+                </div>
+            </div>
+
             <InfoDisplay collection="Front_Desk" />
         </div>
         <LanguageSwitcher />
