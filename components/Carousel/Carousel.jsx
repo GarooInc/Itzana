fec1b264dc41642f9a43c4ce613bc687a68a84dc
Jimena Hernández <her21199@uvg.edu.gt>
2024-10-18 10:51:26 -0600

    Update Carousel.jsx

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -1,39 +1,49 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { CgArrowLongRight } from "react-icons/cg"
 import { CgArrowLongLeft } from "react-icons/cg"
+import { motion, AnimatePresence } from 'framer-motion'
 
-const Carousel = ({images}) => {
+const Carousel = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0)
 
-  return (
-<div className="carousel w-full h-full">
-    {images.map((image, index) => (
-        <div id={`slide-${index}`} key={index} className="carousel-item relative w-full">
-            <img src={image} alt={`slide-${index}`} className="w-full object-cover" />
-            <div className="absolute top-1/2 flex -translate-y-1/2 transform justify-between">
-                {
-                    index !== 0 && 
-                    <button 
-                        className="absolute bg-aqua text-white p-2 rounded-full left-5"
-                        onClick={() => document.getElementById(`slide-${index - 1}`).scrollIntoView(false)}
-                    >
-                        <CgArrowLongLeft className='text-2xl text-cream' />
-                    </button>
-                }
-                {
-                    index !== images.length - 1 && 
-                    <button 
-                        className="absolute bg-aqua text-white p-2 rounded-full right-5"
-                        onClick={() => document.getElementById(`slide-${index}`).scrollIntoView(false)}
-                    >
-                        <CgArrowLongRight className='text-2xl text-cream' />
-                    </button>
-                }
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
+    }
+
+    return (
+        <div className="carousel w-full h-full relative">
+            <AnimatePresence>
+                <motion.img
+                    key={currentIndex}
+                    src={images[currentIndex]}
+                    alt={`slide-${currentIndex}`}
+                    className="w-full h-full object-cover"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                />
+            </AnimatePresence>
+            <div className="absolute top-1/2 flex -translate-y-1/2 transform justify-between w-full">
+                <button
+                    className="absolute bg-aqua text-white p-2 rounded-full left-5"
+                    onClick={handlePrev}
+                >
+                    <CgArrowLongLeft className='text-2xl text-cream' />
+                </button>
+                <button
+                    className="absolute bg-aqua text-white p-2 rounded-full right-5"
+                    onClick={handleNext}
+                >
+                    <CgArrowLongRight className='text-2xl text-cream' />
+                </button>
             </div>
-        </div> 
-    ))}
-</div>
-  )
+        </div>
+    )
 }
 
 export default Carousel
