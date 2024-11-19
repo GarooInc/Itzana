8add717c8c86b5f13e2363d2aa6a9a4c5c692a80
Jimena Hernández <her21199@uvg.edu.gt>
2024-11-19 08:55:11 -0600

    trip advisor embeed

--- app/[locale]/adventures/page.jsx
+++ app/[locale]/adventures/page.jsx
@@ -6,6 +6,7 @@ import FooterCart from '@/components/FooterCart/FooterCart'
 import Image from 'next/image'
 import LanguageSwitcher from '@/components/LanguageSwitcher/LanguageSwitcher'
 import TabCartItem from '@/components/TabCartItem/TabCartItem'
+import TripAdvisorEmbed from '@/components/TripAdvisorEmbed/TripAdvisorEmbed'
 
 
 const namespaces = ['adventures', 'header']
@@ -15,7 +16,7 @@ export default async function Adventures({ params: { locale }}) {
 
 return (
     <TranslationsProvider locale={locale} namespaces={namespaces} resources={resources}>
-        <div className="page bg-white md:px-20 px-10 relative">
+        <div className="page bg-white md:px-20 px-10 relative pb-40">
             <ArrowBack absolute/>
             <div className='flex flex-col justify-center items-center'>
                 <img src="/assets/images/logo_v1.png" alt="logo" className="w-[150px]" />
@@ -35,6 +36,8 @@ return (
             </div>
             <LanguageSwitcher />
             <TabCartItem collection={"adventures"} />
+            <span className="font-futura text-lg text-black py-2">{t('adventures:text3')}</span>
+            <TripAdvisorEmbed />
             <FooterCart />
         </div>
     </TranslationsProvider>
